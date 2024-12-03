305d12e0765989dc64b4478e61633796816a0ae3
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-03 21:13:23 +0530

    fix(src/App.tsx) : remove unused Navlink component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Register from './pages/Register';
 import Login from './pages/Login';
