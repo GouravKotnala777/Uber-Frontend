9db2675d10cc6a847be6a0e6ef1cfb9e8619e987
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-04 15:34:21 +0530

    feat(Start.tsx) : add Start page

---
+++ src/pages/Start.tsx
@@ -0,0 +1,26 @@
+import "../styles/pages/start.scss";
+import bg from "../../public/bg-3.jpg";
+import logo from "../../public/uber-logo-1.png";
+import { FaArrowRight } from "react-icons/fa6";
+
+
+const Start = () => {
+
+
+    return(
+        <div className="start_page_bg">
+            <div className="bg_image_cont">
+                <img className="logo" src={logo} alt={logo} />
+                <img className="bg_img" src={bg} alt={bg} />
+            </div>
+            <div className="continue_cont">
+                <div className="heading">Get started with Uber</div>
+                <div className="continue_btn">
+                    <button><span>Continue</span><FaArrowRight className="BiRightArrow" /></button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Start;
