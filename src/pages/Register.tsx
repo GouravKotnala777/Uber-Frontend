bf5b89f4c081f79b8a96b7b156ace2d56dbfd55c
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-10 14:30:00 +0530

    feat(Register.tsx) : connect register page to backend

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,25 +1,50 @@
 import "../styles/pages/login.scss";
 import logo from "../../public/uber-logo-1.png";
 import { Link } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
+import { RegisterBodyTypes, UserTypes } from "../utils/types";
+import { register } from "../api";
 
 const Register = () => {
+    const [registerFormData, setRegisterFormData] = useState<RegisterBodyTypes>({firstName:"", lastName:"", email:"", password:"", mobile:"", gender:"male"})
+    const [test, setTest] = useState<UserTypes|null>(null);
+    
+
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement|HTMLSelectElement>) => {
+        setRegisterFormData({...registerFormData, [e.target.name]:e.target.value});
+    };
+
+    const onClickHandler = async() => {
+        const registerD = await register(registerFormData);
+        setTest(registerD.jsonData);
+    };
 
     return(
         <div className="register_page_bg">
+            <pre>{JSON.stringify(test, null, `\t`)}</pre>
             <div className="logo_cont">
                 <img src={logo} alt={logo} />
             </div>
             <div className="form_cont">
                 <label>What's your name</label>
                 <div className="fullname_cont">
-                    <input type="text" className="first_name" name="firstName" placeholder="First Name" />
-                    <input type="text" className="first_name" name="lastName" placeholder="Last Name" />
+                    <input type="text" className="first_name" name="firstName" placeholder="First Name"  onChange={(e) => onChangeHandler(e)} />
+                    <input type="text" className="first_name" name="lastName" placeholder="Last Name" onChange={(e) => onChangeHandler(e)} />
                 </div>
                 <label>What's your email</label>
-                <input type="text" name="fullName" placeholder="Email" />
+                <input type="text" name="email" placeholder="Email" onChange={(e) => onChangeHandler(e)} />
                 <label>Enter your password</label>
-                <input type="text" name="password" placeholder="Password" />
-                <button className="register_btn">Register</button>
+                <input type="text" name="password" placeholder="Password" onChange={(e) => onChangeHandler(e)} />
+                <label>Enter your mobile number</label>
+                <input type="text" name="mobile" placeholder="Mobile" onChange={(e) => onChangeHandler(e)} />
+                <label>Enter your gender</label>
+                <select className="gender_select" onChange={(e) => onChangeHandler(e)}>
+                    <option value="">Select</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+                <button className="register_btn" onClick={onClickHandler}>Register</button>
                 <p>Already have a account? <Link to="/user/login" className="link"> Login here</Link></p>
                 <p className="bottom_line">this site is protected by reCAPTCHA and the Google Policy and the Terms of Service apply </p>
                 
