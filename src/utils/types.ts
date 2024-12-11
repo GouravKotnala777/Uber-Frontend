45ad5510f28cf99c8fd0d702df3096a274e6ed4e
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-11 14:37:50 +0530

    feat(types.ts) : add DriverTypes, VehicleTypeTypes and RegisterDriverBodyTypes

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,10 +8,33 @@ export interface UserTypes {
     gender:"male"|"female"|"other";
     role:"user"|"admin";
 };
+export type VehicleTypeTypes = "car"|"motorcycle"|"auto";
+export interface DriverTypes {
+    _id:string;
+    userID:string;
+    licenseNumber:string;
+    vehicleDetailes:{
+        vehicleType:VehicleTypeTypes;
+        vehicleModel:string;
+        vehicleNumber:string;
+        vehicleColor:string;
+    },
+    availabilityStatus:boolean;
+    rating:number;
+    createdAt:Date;
+    updatedAt:Date;
+};
 
 export interface RegisterBodyTypes extends Pick<UserTypes, "email"|"mobile"|"gender"> {
     firstName:string;
     lastName:string;
+    password:string;
+};
+export interface RegisterDriverBodyTypes extends Pick<DriverTypes, "licenseNumber"|"userID"> {
+    vehicleColor:string;
+    vehicleModel:string;
+    vehicleNumber:string;
+    vehicleType:VehicleTypeTypes;
     password:string;
 };
 export interface ResponseType<J> {
