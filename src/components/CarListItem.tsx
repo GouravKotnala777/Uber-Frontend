8139590cac6af0b0c7e5adb5d9ae12f2cf8cabd1
gouravkotnala <gouravkotnala777@gmail.com>
2024-12-13 22:27:22 +0530

    refactor(CarListItem.tsx) : pass allFare and vehicleType as props

--- src/components/CarListItem.tsx
+++ src/components/CarListItem.tsx
@@ -1,19 +1,20 @@
 import "../styles/components/car_list_item.scss";
 import { BiUser } from "react-icons/bi";
 import car from "../../public/car1.png";
+import { VehicleTypeTypes } from "../utils/types";
 
 
-const CarListItem = (carDetailes:{name:string; passengersCapacity:number; away:string; feature:string; price:number;}) => {
+const CarListItem = ({allFare, vehicleType}:{vehicleType:VehicleTypeTypes; allFare:{auto:number; car:number; motorcycle:number;}}) => {
 
     return(
         <div className="list_item">
             <div className="car_icon_cont"><img src={car} alt={car} /></div>
             <div className="details_cont">
-                <div className="name">{carDetailes.name} <div className="passengers_capacity"><BiUser /> {carDetailes.passengersCapacity}</div></div>
-                <div className="away">{carDetailes.away}</div>
-                <div className="away1">{carDetailes.feature}</div>
+                <div className="name">{vehicleType} <div className="passengers_capacity"><BiUser /> {2}</div></div>
+                <div className="away">{"2 minutes away 15.24"}</div>
+                <div className="away1">{"affordable compact"}</div>
             </div>
-            <div className="price_cont">₹{carDetailes.price}</div>
+            <div className="price_cont">₹{allFare[vehicleType]}</div>
         </div>
     )
 };
