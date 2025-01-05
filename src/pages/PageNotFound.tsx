8db6dc38d428b2b0a5e0a703e72b28388715871b
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-05 18:58:01 +0530

    style(PageNotFound.tsx) : add stylings

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,9 @@
+import "../styles/pages/page_not_found.scss";
 import { useNavigate } from "react-router-dom";
 import Heading from "../components/Heading";
+import img from "/public/page-not-found8.png";
+import { CenterContainer } from "../components/WrapperContainers";
+import Button from "../components/Button";
 
 
 const PageNotFound = () => {
@@ -7,8 +11,13 @@ const PageNotFound = () => {
 
     return(
         <div className="page_not_found">
-            <Heading text="Page not found" fontSize="1.3rem" />
-            <button onClick={() => navigate("/user/home")}>Go back home</button>
+            <CenterContainer>
+                <Heading text="Page not found" fontSize="1.5rem" />
+            </CenterContainer>
+            <CenterContainer>
+                <img src={img} alt={img} />
+            </CenterContainer>
+            <Button text="Go back to home" onClickHandler={() => navigate("/user/home")} />
         </div>
     )
 };
