634c742e29e060587b198f7c037597db64b99160
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-17 13:32:00 +0530

    feat(WrapperContainers.tsx) : add InfiniteScroller component

--- src/components/WrapperContainers.tsx
+++ src/components/WrapperContainers.tsx
@@ -1,11 +1,21 @@
 import "../styles/components/scrollable_container.scss";
-import { ReactNode } from "react";
+import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
+import { ResponseType } from "../utils/types";
+import Spinner from "./Spinner";
 
 
 interface ScrollableContainerPropTypes{
     children:ReactNode;
     height?:string;
     width?:string;
+};
+interface InfiniteScrollerPropTypes{
+    children:ReactNode;
+    height?:string;
+    width?:string;
+    api:(skip:number) => Promise<ResponseType<any>>;
+    wholeArray:any[];
+    setWholeArray:Dispatch<SetStateAction<any[]>>;
 };
 
 export const ScrollableContainer = ({children, height, width}:ScrollableContainerPropTypes) => {
@@ -42,6 +52,44 @@ export const Panel = ({children, isPanelActive, onClosePosition, hasRideAccepted
         }}
         >
             {children}
+        </div>
+    )
+};
+export const InfiniteScroller = ({children, height, width, wholeArray, setWholeArray, api}:InfiniteScrollerPropTypes) => {
+    const [skip, setSkip] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const apiHandler = async() => {
+        setIsLoading(true);
+        const res = await api(skip);
+
+        if (res.success) {
+            setIsLoading(false);
+            if (res.jsonData.length !== 0) {
+                setWholeArray([...wholeArray, ...res.jsonData]);
+                setSkip((prev) => prev+1);
+            }
+        }
+        else{
+            setIsLoading(false);
+        }
+
+    };
+
+    useEffect(() => {
+        apiHandler();
+    }, []);
+
+    return(
+        <div className="infinite_scroller_cont" style={{height, width}}>
+            <div className="scroller_cont">
+                {
+                    children
+                }
+                <div className="next_btn_cont">
+                    <button onClick={apiHandler}>{isLoading ? <Spinner width="18px" border="3px solid white" borderTop="3px solid black" /> : "next"}</button>
+                </div>
+            </div>
         </div>
     )
 };
