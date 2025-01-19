69d4625eb5d09736cf6d8fadcee9ef54245e712c
gouravkotnala <gouravkotnala777@gmail.com>
2025-01-19 22:21:53 +0530

    style(ChatPanel.tsx) : add style

--- src/components/ChatPanel.tsx
+++ src/components/ChatPanel.tsx
@@ -26,6 +26,7 @@ const ChatPanel = ({isChatPanelActive, setIsChatPanelActive, receiver, senderTyp
         const newChat = await createChat({receiver, content:messageInp, senderType, receiverSocketID});
 
         if (newChat.success) {
+            setMessageInp("");
             setMessages((prev) => [...prev, newChat.jsonData]);
         }
     };
@@ -47,38 +48,99 @@ const ChatPanel = ({isChatPanelActive, setIsChatPanelActive, receiver, senderTyp
                 <div id="messanger_screen_cont" className="messanger_screen_cont">
                     <div className="messages_cont">
                         {
-                            messages.map((msg) => {
-                                if (msg.receiver === myUserID) {
-                                    return(
-                                        <div className="incoming_message">
-                                            {msg.content}
-                                        </div>
-                                    )
-                                }
-                                else if (msg.sender === myUserID){
-                                    return(
-                                        <div className="outgoing_message">
-                                            {msg.content}
-                                        </div>
-                                    )
-                                }
-                                else{
-                                    return(
-                                        <div className="outgoing_message">
-                                            Pata nahi kuch gadbad ho gai
-                                            receiver = {msg.receiver}
-                                            sender = {msg.sender}
-                                        </div>
-                                    )
-                                }
-                            })
+                            //messages.map((msg) => {
+                            //    if (msg.receiver === myUserID) {
+                            //        return(
+                            //            <div className="incoming_message">
+                            //                {msg.content}
+                            //            </div>
+                            //        )
+                            //    }
+                            //    else if (msg.sender === myUserID){
+                            //        return(
+                            //            <div className="outgoing_message">
+                            //                {msg.content}
+                            //            </div>
+                            //        )
+                            //    }
+                            //    else{
+                            //        return(
+                            //            <div className="outgoing_message">
+                            //                Pata nahi kuch gadbad ho gai
+                            //                receiver = {msg.receiver}
+                            //                sender = {msg.sender}
+                            //            </div>
+                            //        )
+                            //    }
+                            //})
                         }
+
+
+
+<div className="incoming_message">
+                            asdasasd1
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj2
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd3
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj4
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd5
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj8
+                        </div>
+                        <div className="incoming_message">
+                            asdasasd9
+                        </div>
+                        <div className="outgoing_message">
+                            kjcxkhj10
+                        </div>
                     </div>
                 </div>
                 <div className="messanger_controllers_cont">
                     <div className="input_cont">
                         <input type="text" className="message_inp" placeholder="Send a message..." onChange={(e) => setMessageInp(e.target.value)} />
-                        <button className="send_message_btn" onClick={createChatHandler}><BiSend className="BiSend" /></button>
+                        {
+                            messageInp &&
+                                <button className="send_message_btn" onClick={createChatHandler}><BiSend className="BiSend" /></button>
+                        }
+
                     </div>
                 </div>
             </div>
